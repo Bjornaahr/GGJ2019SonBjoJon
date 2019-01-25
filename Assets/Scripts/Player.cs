866263928c94ae6f8936157c711a42997677fac8
Bjørnar Hammerseth <bjornar.hammerseth@gmail.com>
2019-01-25 18:19:50 +0100

    Added player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float Food;
+    public float Temprature;
+
+    [SerializeField]
+    float speed;
+    float windSpeed;
+    float velocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
