b7d987b0ee2c824fce16a8676c4d00a9c01704d2
bjornaah <bjornaah@stud.ntnu.no>
2019-01-25 23:50:39 +0100

    Player movment and camera follow, started work on wind, food and temp

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField]
+    float height, distance, offsetX, smoothTime;
+    [SerializeField]
+    Transform player;
+    Vector3 velocity = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetPos = new Vector3(player.position.x + offsetX, height, distance);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,82 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float Food;
+    public float Food;      
     public float Temprature;
 
     [SerializeField]
-    float speed;
-    float windSpeed;
-    float velocity;
+    float speed, jumpSpeed, windSpeed, acceleration;
+
+    Rigidbody2D rigidbod;
+    bool isGrounded = true;
+
+    [SerializeField]
+    Transform rayGround;
+
+    float startTime;
+    float timeNow;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbod = GetComponent<Rigidbody2D>();
+        startTime = Time.time;
+        Food = 100;
+        Temprature = 37;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeNow = Time.time - startTime;
+        Movement();
+        CheckGround();
+        Wind();
+        FoodDecrease();
+        TempratureDecrease();
     }
+
+    void Movement()
+    {
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rigidbod.velocity = Vector2.up * jumpSpeed;
+        }
+
+        speed = Input.GetAxis("Horizontal") * acceleration;
+        rigidbod.velocity = new Vector2(speed - windSpeed, rigidbod.velocity.y);
+
+        //rigidbod.AddForce(transform.right * -windSpeed);
+
+    }
+
+    void CheckGround()
+    {
+        RaycastHit2D hitDown = Physics2D.Raycast(rayGround.position, Vector2.down, 0.1f);
+
+        if (hitDown)
+        {
+            isGrounded = true;
+        } else isGrounded = false;
+    }
+
+    //Increases wind with respect to time
+    void Wind()
+    {
+        windSpeed = (timeNow / 1000) + 1;
+    }
+
+    //Decreases food
+    void FoodDecrease()
+    {
+        Food -= (timeNow / 500);
+    }
+
+    //Decreases temprature while not near a fireplace
+    void TempratureDecrease()
+    {
+        Temprature -= (timeNow / 5000);
+    }
+
 }
